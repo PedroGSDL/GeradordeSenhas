40617e5dcca3fb537c81fcbc5c1203ddfe5ce799
Pedro Gabriel Santos de Lima <116593548+PedroGSDL@users.noreply.github.com>
2024-10-14 10:38:05 -0300

    Update script.js
    Pequenas alterações

--- script.js
+++ script.js
@@ -1,28 +1,11 @@
-//Seleciona o slider que define o tamanho da senha
 let sliderElement = document.querySelector("#slider");
-
-//Seleciona o botão que aciona a geração da senha
 let buttonElement = document.querySelector("#button");
-
-//Seleciona o elemento que exibe o valor do slider
 let sizePassword = document.querySelector("#valor");
-
-//Seleciona o elemento onde a senha gerada será exibida
 let password = document.querySelector("#password");
-
-//Seleciona o container onde a senha gerada será exibida
 let containerPassword = document.querySelector("#container-password");
-
-//Conjunto de caracteres para gerar a senha
 let charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&*';
-
-//Variável que armazenará a senha gerada
 let novaSenha = '';
-
-//Exibe o valor inicial do slider
 sizePassword.innerHTML = sliderElement.value;
-
-//Atualiza o valor exibido quando o slider é ajustado
 slider.oninput = function() 
 {
   sizePassword.innerHTML = this.value;
